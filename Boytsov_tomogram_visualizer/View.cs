ec13bba5d771f277eb19088e49e5894862ef767a
BoytsovVA <vladboi8@gmail.com>
2020-05-17 23:54:15 +0300

    First

---
+++ Boytsov_tomogram_visualizer/View.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boytsov_tomogram_visualizer
+{
+    class View
+    {
+    }
+}
